5a39265b672e3cea01328e8c3047210ef9d13c64
Ekaterina Pashina <k377at@mail.ru>
2024-04-04 14:59:57 +0500

    added gif to missing page

--- letter/src/components/header/header.jsx
+++ letter/src/components/header/header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Logo from '../logo/logo';
 import './header.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default class Header extends React.Component {
     render() {
--- letter/src/components/missing/missing.jsx
+++ letter/src/components/missing/missing.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './missing.css';
+import { Link } from 'react-router-dom';
 
 function Missing() {
     return (
         <>
-            <h1 className="lostPage">Страничка потерялась</h1>
+            <h1 className="lostPage">Упс! Кажется, такой страницы не существует!</h1>
+            <div className="lostPage__img">
+                <img src="https://media.tenor.com/PGnGqfkIuLYAAAAM/cat-head.gif" alt="Грустный кот" />
+            </div>
+            <p className="lostPage__return">Попробуйте вернуться на {<Link to="/">Главную страницу</Link>}</p>
         </>
     )
 }
